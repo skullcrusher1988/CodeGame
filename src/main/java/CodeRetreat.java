3385a30512cff7cfc47b1d687e88f2d1a28806d7
skullcrusher1988 <maheshoxford2007@gmail.com>
2021-10-20 09:11:50 +0530

    Initial commit1

--- src/main/java/CodeRetreat.java
+++ src/main/java/CodeRetreat.java
@@ -1,37 +1,9 @@
+import java.util.Arrays;
 
 public class CodeRetreat {
 	
 	public static void main(String[] arr) {
-		System.out.print("startClass");
-		 int M = 10, N = 10;
-		 
-	        // Intiliazing the grid.
-	        int[][] grid = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
-	            { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
-	            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
-	            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-	        };
-	 
-	        // Displaying the grid
-	        System.out.println("Original Generation");
-	        for (int i = 0; i < M; i++)
-	        {
-	            for (int j = 0; j < N; j++)
-	            {
-	                if (grid[i][j] == 0)
-	                    System.out.print(".");
-	                else
-	                    System.out.print("*");
-	            }
-	            System.out.println();
-	        }
-	        System.out.println();
+
 	}
 
 }
